c44016ec36416ce57ad41b4cbad0ce961e0d7f00
AlexYud <alexandre.yudi.oliveira@gmail.com>
2025-08-10 22:19:59 -0300

    add AuthTokenGuard for JWT authentication

---
+++ src/auth/guard/auth-token.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export class AuthTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request.user = payload;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
