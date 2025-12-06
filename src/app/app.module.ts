d3a440f70b623ee378c3725aee2d52598217c084
AlexYud <alexandre.yudi.oliveira@gmail.com>
2025-12-06 20:12:01 -0300

    fix: remove unused import of Req from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule, Req, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from 'src/tasks/tasks.module';
